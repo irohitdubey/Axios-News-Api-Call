37411ad05dffcfeae1d307d00e4cb136b3d9f0bc
irohitdubey <ceo.rohitdubey@gmail.com>
2022-06-16 11:27:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import FetchNews from './Components/FetchNews'
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <FetchNews/>
+     
+
+    </>
+  
+  );
+}
+
+export default App;
